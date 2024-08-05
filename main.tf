6dcc826b30c461bca954017ef27639097deaeb6e
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-08-05 18:13:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "my-subnet"
   }
