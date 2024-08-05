64d5540a329fcd089ec04b2777e8965c1d7262b4
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-08-05 18:26:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_instance" "my_instance1" {
+resource "aws_instance" "my_instance1.1" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   key_name      = "NNnn"
@@ -32,7 +32,7 @@ resource "aws_instance" "my_instance1" {
   }
 }
 
-resource "aws_instance" "my_instance2" {
+resource "aws_instance" "my_instance1.2" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.medium"
   key_name      = "NNnn"
@@ -42,9 +42,9 @@ resource "aws_instance" "my_instance2" {
   }
 }
 
-resource "aws_instance" "my_instance3" {
+resource "aws_instance" "my_instance1.3" {
   ami           = "ami-04a81a99f5ec58529"
-  instance_type = "t2.micro"
+  instance_type = "t3.medium"
   key_name      = "NNnn"
   subnet_id     = aws_subnet.my_subnet.id
   tags = {
