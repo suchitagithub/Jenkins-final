510425dc12bce8128aed691ba182db4ee7af31a6
Sonal-BP <108402618+Sonal-BP@users.noreply.github.com>
2024-08-07 11:52:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,29 @@
-variable "region" {
-  default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-  default = "10.0.1.0/24"
+  description = "CIDR block for subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "ami_id" {
+  description = "AMI ID for instances"
+  default     = "ami-0497a974f8d5dcef8"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for subnet"
+  default     = "us-east-1a"
+}
+
+variable "key_name" {
+  description = "Name of the key pair to use for SSH access"
+  default     = "NNnn"
 }
